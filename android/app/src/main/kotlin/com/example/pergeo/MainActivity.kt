24f84da05d189512ea7c763c59e8c8289f747f0b
Die4uSixx <101938478+Die4uSixx@users.noreply.github.com>
2024-10-03 20:32:53 -0300

    asa

--- android/app/src/main/kotlin/com/example/pergeo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pergeo/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.pergeo
+package com.example.myapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
