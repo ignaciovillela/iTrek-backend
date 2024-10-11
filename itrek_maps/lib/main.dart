af0e24f6e97cbd1b2bf9d3083e7d0eb6bba72edb
Die4uSixx <101938478+Die4uSixx@users.noreply.github.com>
2024-10-10 22:00:32 -0300

    Update main.dart

--- itrek_maps/lib/main.dart
+++ itrek_maps/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: GoogleMapsPage(), // Llama a la nueva página de Google Maps
+      home: LoginScreen(), // Llama a la nueva página de Google Maps
     );
   }
 }
