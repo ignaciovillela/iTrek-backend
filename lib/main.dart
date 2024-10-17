05b72ab29d1ab67dcf5f70e256010d8189692b46
Die4uSixx <101938478+Die4uSixx@users.noreply.github.com>
2024-10-17 10:01:10 -0300

    Token

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importa dotenv
 import 'package:itrek_maps/pages/inicio.dart';
 import 'package:itrek_maps/pages/login.dart';
+import 'package:itrek_maps/pages/maps_google.dart';
 
 import 'pages/listadoRutas.dart';
 
@@ -16,6 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginScreen());
+    return const MaterialApp(home: GoogleMapsPage());
   }
 }
--- lib/pages/inicio.dart
+++ lib/pages/inicio.dart
@@ -1,213 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:itrek_maps/DataBase/bd_itrek.dart'; // Asegúrate de tener la ruta correcta de la clase DatabaseHelper
 
 import 'comunidad.dart'; // Importa la pantalla de comunidad
 import 'listadoRutas.dart'; // Importamos la pantalla de listado de rutas
 import 'maps_google.dart'; // Importamos la pantalla del mapa
 import 'perfil.dart'; // Importa la pantalla de perfil
+import 'login.dart'; // Pantalla de login para redireccionar si no hay token
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
+  Future<bool> _checkToken() async {
+    final dbHelper = DatabaseHelper.instance;
+    // Busca en la base de datos si existe un usuario con un token válido
+    List<Map<String, dynamic>> userWithToken = await dbHelper.getUserWithToken();
+
+    // Si encuentra un usuario con token, el usuario está autenticado
+    if (userWithToken.isNotEmpty) {
+      return true; // Usuario autenticado
+    } else {
+      return false; // Usuario no autenticado
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF50C9B5), // Color de fondo del AppBar
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/images/logo.png', // Asegúrate de que el logo esté en la carpeta assets
-              height: 30, // Tamaño pequeño del logo
-            ),
-            const SizedBox(width: 10), // Espacio entre el logo y el texto
-            const Text(
-              'iTrek',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+    return FutureBuilder<bool>(
+      future: _checkToken(), // Verifica el token antes de construir la interfaz
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasData && snapshot.data == true) {
+          // Si el token es válido, muestra la pantalla de menú
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color(0xFF50C9B5),
+              title: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png', // Asegúrate de que el logo esté en la carpeta assets
+                    height: 30,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'iTrek',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          const SizedBox(height: 40), // Espacio para bajar el contenido
-
-          // Texto que dice "Menú"
-          const Text(
-            'Menú',
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-
-          const SizedBox(height: 20), // Espacio debajo del texto
-
-          // Imagen desde assets en la parte superior central
-          Center(
-            child: Image.asset(
-              'assets/images/trek.png', // Asegúrate de que la imagen esté en assets
-              height: 120, // Tamaño de la imagen
-            ),
-          ),
-
-          const SizedBox(height: 50), // Espacio debajo de la imagen
-
-          Expanded(
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: GridView.count(
-                  shrinkWrap: true,
-                  crossAxisCount:
-                      2, // Número de columnas (2 para formar un cuadrado)
-                  crossAxisSpacing: 15.0, // Menor espacio entre columnas
-                  mainAxisSpacing: 15.0, // Menor espacio entre filas
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color(0xFF50C9B5), // Color del botón
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(10), // Bordes redondeados
-                        ),
-                        padding:
-                            const EdgeInsets.all(10), // Botones más compactos
-                      ),
-                      onPressed: () {
-                        // Navega a la pantalla del mapa al presionar el botón "Iniciar Ruta"
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const GoogleMapsPage()),
-                        );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/maps-green.png', // Imagen asociada al botón
-                            height: 120, // Tamaño ajustado de la imagen
-                          ),
-                          const SizedBox(
-                              height: 10), // Espacio entre imagen y texto
-                          const Text('Iniciar Ruta'),
-                        ],
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color(0xFF50C9B5), // Color del botón
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(10), // Bordes redondeados
-                        ),
-                        padding:
-                            const EdgeInsets.all(10), // Botones más compactos
-                      ),
-                      onPressed: () {
-                        // Navega a la pantalla de perfil
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const PerfilUsuarioScreen()),
-                        );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/perfil.png', // Imagen asociada al botón
-                            height: 120, // Tamaño ajustado de la imagen
-                          ),
-                          const SizedBox(
-                              height: 10), // Espacio entre imagen y texto
-                          const Text('Perfil'),
-                        ],
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color(0xFF50C9B5), // Color del botón
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(10), // Bordes redondeados
-                        ),
-                        padding:
-                            const EdgeInsets.all(10), // Botones más compactos
-                      ),
-                      onPressed: () {
-                        // Navega a la pantalla de listado de rutas
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ListadoRutasScreen()),
-                        );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/listado.png', // Imagen asociada al botón
-                            height: 120, // Tamaño ajustado de la imagen
-                          ),
-                          const SizedBox(
-                              height: 10), // Espacio entre imagen y texto
-                          const Text('Listado de Rutas'),
-                        ],
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color(0xFF50C9B5), // Color del botón
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(10), // Bordes redondeados
-                        ),
-                        padding:
-                            const EdgeInsets.all(10), // Botones más compactos
-                      ),
-                      onPressed: () {
-                        // Navega a la pantalla de comunidad
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const RutasCompartidasScreen()),
-                        );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/com.png', // Imagen asociada al botón
-                            height: 120, // Tamaño ajustado de la imagen
-                          ),
-                          const SizedBox(
-                              height: 10), // Espacio entre imagen y texto
-                          const Text('Comunidad'),
-                        ],
-                      ),
-                    ),
-                  ],
+            body: Column(
+              children: [
+                const SizedBox(height: 40),
+                const Text(
+                  'Menú',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
+                Center(
+                  child: Image.asset(
+                    'assets/images/trek.png',
+                    height: 120,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: GridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 15.0,
+                        mainAxisSpacing: 15.0,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF50C9B5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const GoogleMapsPage()),
+                              );
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/maps-green.png',
+                                  height: 120,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Iniciar Ruta'),
+                              ],
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF50C9B5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const PerfilUsuarioScreen()),
+                              );
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/perfil.png',
+                                  height: 120,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Perfil'),
+                              ],
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF50C9B5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const ListadoRutasScreen()),
+                              );
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/listado.png',
+                                  height: 120,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Listado de Rutas'),
+                              ],
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF50C9B5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const RutasCompartidasScreen()),
+                              );
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/com.png',
+                                  height: 120,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Comunidad'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+              ],
             ),
-          ),
-
-          const SizedBox(height: 40), // Más espacio para bajar los botones
-        ],
-      ),
+          );
+        } else {
+          // Si no hay token, redirige al login
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
